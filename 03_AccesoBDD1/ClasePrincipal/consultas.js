ae95df360cb3bad6510b962d94c1087b2a6a51f3
Claudia Villarroel <claudia.villarroel@live.com>
2024-01-25 23:54:03 -0300

    :hammer: Clase principal metodo agregar

--- 03_AccesoBDD1/ClasePrincipal/consultas.js
+++ 03_AccesoBDD1/ClasePrincipal/consultas.js
@@ -1,14 +1,20 @@
-const {pool} = require('./config/db');
+const { pool } = require('./config/db');
 
-const getBooks = async (name,editorial,autor,precio) => {
+const getBooks = async (name, editorial, autor, precio) => {
+  const { rows } = await pool.query('SELECT * FROM books');
+  console.log(rows);
+  return rows;
+};
 
-    const {rows} = await pool.query(
-        "SELECT * FROM books"
-    );
-    console.log(rows);
-    return rows;
-}
+const addBook = async (name, editorial, autor, precio) => {
+  const { rows } = await pool.query(
+    'INSERT INTO books (name, editorial, autor, precio) VALUES ($1, $2, $3, $4)',
+    [name, editorial, autor, precio]
+  );
+  return rows;
+};
 
 module.exports = {
-    getBooks
-}
+  getBooks,
+  addBook,
+};
--- 03_AccesoBDD1/ClasePrincipal/routes/routes.js
+++ 03_AccesoBDD1/ClasePrincipal/routes/routes.js
@@ -1,18 +1,24 @@
 const express = require('express');
-const { getBooks } = require('../consultas');
+const { getBooks, addBook } = require('../consultas');
 const router = express.Router();
 
-
-router.get('/', function(req, res){
-    res.send('Hola Mundo');
+router.get('/', function (req, res) {
+  res.send('Hola Mundo');
 });
 
-router.get('/users', function(req, res){
-    res.send('Listado de usuarios');
-})
+router.get('/users', function (req, res) {
+  res.send('Listado de usuarios');
+});
 
-router.get('/books', async function(req, res){
-    const result = await getBooks();
-    res.json(result);
-})
-module.exports = router
+router.get('/books', async function (req, res) {
+  const result = await getBooks();
+  res.json(result);
+});
+
+router.post('/books', async function (req, res) {
+  const { name, editorial, autor, precio } = req.body;
+  console.log(name, editorial, autor, precio);
+  const result = await addBook(name, editorial, autor, precio);
+  res.send('Libro agregado');
+});
+module.exports = router;
